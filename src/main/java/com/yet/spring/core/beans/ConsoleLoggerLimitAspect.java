7909631dce34ff6b12864a19066fe6805e717c30
Stanislav Iskortsev <iskortsevsv@gmail.com>
2021-06-22 14:51:06 +0200

    Added ConsoleLoggerLimitAspect limit aspect

---
+++ src/main/java/com/yet/spring/core/beans/ConsoleLoggerLimitAspect.java
@@ -0,0 +1,27 @@
+package com.yet.spring.core.beans;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+
+public class ConsoleLoggerLimitAspect {
+    private final int maxCount;
+
+    private final EventLogger otherLogger;
+
+    private int currentCount = 0;
+
+    public ConsoleLoggerLimitAspect(int maxCount, EventLogger otherLogger) {
+        this.maxCount = maxCount;
+        this.otherLogger = otherLogger;
+    }
+
+    public void aroundLogEvent(ProceedingJoinPoint jp, Event evt) throws Throwable {
+        if (currentCount < maxCount) {
+            System.out.println("ConsoleEventLogger max count is not reached. Continue...");
+            currentCount++;
+            jp.proceed(new Object[] {evt});
+        } else {
+            System.out.println("ConsoleEventLogger max count is reached. Logging to " + otherLogger.getName());
+            otherLogger.logEvent(evt);
+        }
+    }
+}
